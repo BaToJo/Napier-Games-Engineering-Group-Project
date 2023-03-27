7e527742d099f542f072c72f074fc61d4a5d9ef4
BaToJo <57232803+BaToJo@users.noreply.github.com>
2023-03-27 21:16:09 +0100

    Updated world textures; begun test map
    The test map "city" was made in the "Tiled" software. The .tmx file can be opened in "Tiled".

---
+++ res/textures/tilemap.tsx
@@ -0,0 +1,95 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="tilemap" tilewidth="16" tileheight="16" tilecount="30" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <transformations hflip="1" vflip="1" rotate="1" preferuntransformed="0"/>
+ <tile id="24">
+  <image width="16" height="16" source="asphalt.bmp"/>
+ </tile>
+ <tile id="46">
+  <image width="16" height="16" source="asphalt3.png"/>
+ </tile>
+ <tile id="29">
+  <image width="16" height="16" source="bricks.bmp"/>
+ </tile>
+ <tile id="30">
+  <image width="16" height="16" source="cobbles.bmp"/>
+ </tile>
+ <tile id="31">
+  <image width="16" height="16" source="grass.bmp"/>
+ </tile>
+ <tile id="42">
+  <image width="16" height="16" source="grass4.png"/>
+ </tile>
+ <tile id="43">
+  <image width="16" height="16" source="grass3.png"/>
+ </tile>
+ <tile id="44">
+  <image width="16" height="16" source="grass2.png"/>
+ </tile>
+ <tile id="32">
+  <image width="16" height="16" source="mud.bmp"/>
+ </tile>
+ <tile id="39">
+  <image width="16" height="16" source="mud4.png"/>
+ </tile>
+ <tile id="40">
+  <image width="16" height="16" source="mud3.png"/>
+ </tile>
+ <tile id="41">
+  <image width="16" height="16" source="mud2.png"/>
+ </tile>
+ <tile id="33">
+  <image width="16" height="16" source="roof_detailed.bmp"/>
+ </tile>
+ <tile id="34">
+  <image width="16" height="16" source="roof_simple.bmp"/>
+ </tile>
+ <tile id="35">
+  <image width="16" height="16" source="sidewalk.bmp"/>
+ </tile>
+ <tile id="36">
+  <image width="16" height="16" source="sidewalk4.png"/>
+ </tile>
+ <tile id="37">
+  <image width="16" height="16" source="sidewalk3.png"/>
+ </tile>
+ <tile id="38">
+  <image width="16" height="16" source="sidewalk2.png"/>
+ </tile>
+ <tile id="48">
+  <image width="16" height="16" source="asphalt_line.png"/>
+ </tile>
+ <tile id="49">
+  <image width="16" height="16" source="asphalt_line2.png"/>
+ </tile>
+ <tile id="50">
+  <image width="16" height="16" source="asphalt_line3.png"/>
+ </tile>
+ <tile id="51">
+  <image width="16" height="16" source="asphalt_line4.png"/>
+ </tile>
+ <tile id="52">
+  <image width="16" height="16" source="deadgrass.png"/>
+ </tile>
+ <tile id="53">
+  <image width="16" height="16" source="deadgrass2.png"/>
+ </tile>
+ <tile id="54">
+  <image width="16" height="16" source="deadgrass3.png"/>
+ </tile>
+ <tile id="55">
+  <image width="16" height="16" source="deadgrass4.png"/>
+ </tile>
+ <tile id="56">
+  <image width="16" height="16" source="asphalt_lineWhite.png"/>
+ </tile>
+ <tile id="57">
+  <image width="16" height="16" source="asphalt_lineWhite2.png"/>
+ </tile>
+ <tile id="58">
+  <image width="16" height="16" source="asphalt_lineWhite3.png"/>
+ </tile>
+ <tile id="59">
+  <image width="16" height="16" source="asphalt_lineWhite4.png"/>
+ </tile>
+</tileset>
